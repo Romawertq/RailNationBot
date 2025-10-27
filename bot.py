80dff94372f558410b86dbfe9cb5fca8496b47ce
Romawertq <plitynplitin10@gmail.com>
2025-10-27 22:09:39 +0300

    Появление бота на github, возможности на данном этапе: /setting добавляет id темы откуда нужно получать инфу, id темы куда её надо отправлять, id группы; /slave рабочий, деньги, время.

---
+++ bot.py
@@ -0,0 +1,48 @@
+import tracemalloc
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+from datetime import datetime
+from db import (
+    slave1,
+    my_function,
+    setting1
+)
+
+bot = Bot(token="7457602333:AAGK16EQolfpV9IBlXgx923XWOcsOZ3VZ9U")
+dp = Dispatcher(bot, storage=MemoryStorage())
+tracemalloc.start()
+
+async def check_time():
+    minutes_to_run = [0, 45, 50, 54]
+    print('Начало')
+    while True:
+        now = datetime.now()
+        if now.minute in minutes_to_run:
+            g = my_function()
+            w, t, r = g
+            await bot.send_message(chat_id=int(w), text=t, reply_to_message_id=int(r))
+            print(f'Запуск функции, время: {now}')
+            await asyncio.sleep(60)
+
+
+@dp.message_handler(commands=['setting'])
+async def setting(message: types.Message):
+    await setting1(message)
+    await message.answer("Я добавил ваши id")
+
+@dp.message_handler(commands=['slave'])
+async def slave(message: types.Message):
+    g = slave1(message)
+    re, fer, w = g
+    await bot.send_message(chat_id=int(re), text=fer, reply_to_message_id=int(w))
+
+async def on_startup(dp):
+    asyncio.create_task(check_time())
+
+def main():
+    # Запускаем бота
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+
+if __name__ == '__main__':
+    main()
---
+++ db.py
@@ -0,0 +1,123 @@
+import asyncpg
+from datetime import datetime
+
+
+HOST = "127.0.0.1"
+PASSWORD = "postgresEQW1"
+PORT = "5432"
+USER = "tyr"
+DB_NAME = "postgres"
+
+async def fetch_messages(conn):
+    return await conn.fetch("SELECT server, slave, money, time, sent FROM slave WHERE time = $1", datetime.now().strftime('%H:%M'))
+
+async def my_function():
+    conn = await asyncpg.connect(
+            host=HOST,
+            user=USER,
+            password=PASSWORD,
+            database=DB_NAME,
+            port=PORT
+        )
+    messages = await fetch_messages(conn)
+    for record in messages:
+        server_id = record['server']
+        result = await conn.fetch(f"SELECT id_vivod, server_id FROM setting WHERE id_pros = '{server_id}'")
+        if result:
+            sent_value = result[0]['id_vivod']
+            server = result[0]['server_id']
+            t = record['sent']
+            await conn.execute("DELETE FROM slave WHERE sent = $1", t)
+            await conn.close()
+
+            w = server
+            g = sent_value
+            print('Работа и конец функции по передаче')
+            return  w, t, g
+
+async def setting1(message):
+    data = message.text.split()[1:]
+    conn = await asyncpg.connect(
+        host=HOST,
+        user=USER,
+        password=PASSWORD,
+        database=DB_NAME,
+        port=PORT
+    )
+    id_pros = data[0]
+    id_vivod = data[1]
+    server = data[2]
+    print(id_pros, id_vivod, server)
+    await conn.fetch(f"INSERT INTO setting VALUES ('{id_pros}', '{id_vivod}', '{server}')")
+    await conn.close()
+
+async def slave1(message):
+    data = message.text.split()[1:]
+    conn = await asyncpg.connect(
+        host=HOST,
+        user=USER,
+        password=PASSWORD,
+        database=DB_NAME,
+        port=PORT
+    )
+    r = message.reply_to_message
+    der = r = message.reply_to_message.message_id
+    print(der)
+    rows = await conn.fetch("SELECT id_pros, id_vivod, server_id FROM setting WHERE id_pros = $1", str(der))
+
+
+
+    if rows:
+        slave = data[0]
+        money = data[1]
+        time = 0
+        now = datetime.now()
+
+        hours, minutes = time.split(':')
+        hour = int(hours)
+        formatted_time = now.replace(hour=hour, minute=00, second=0, microsecond=0)
+        formatted_time1 = now.replace(hour=hour, minute=45, second=0, microsecond=0)
+        formatted_time2 = now.replace(hour=hour, minute=50, second=0, microsecond=0)
+        formatted_time3 = now.replace(hour=hour, minute=54, second=0, microsecond=0)
+
+        formatted_time34 = f"{hours}:54"
+        formatted_time23 = f"{hours}:50"
+        formatted_time12 = f"{hours}:45"
+        formatted_time11 = f"{hours}:00"
+        we = rows
+        w = we[0].get('id_vivod')
+        re = we[0].get('server_id')
+        print(w)
+        fer = f"@everyone Готовим денег от {money} на {slave}, ставки с {formatted_time11} до {time}"
+        k = f'@everyone Начался аукцион на раба {slave}, ставка от {money}, НЕ СВЕТИМ!!!'
+        k1 = f'@everyone 10 минут!!! {slave}, ставка от {money}, НЕ СВЕТИМ!!!'
+        k2 = f'@everyone 5 минут!!! {slave}, ставка от {money}, НЕ СВЕТИМ!!!'
+        k3 = f'@everyone 1 минута!!! {slave}, ставка от {money}, НЕ СВЕТИМ!!!'
+
+        if formatted_time > now:
+            await conn.execute(
+                f"INSERT INTO slave VALUES ($1, $2, $3, $4, $5);",
+                formatted_time11, slave, money, k, str(der)
+            )
+
+
+        if formatted_time1 > now:
+            await conn.execute(
+                f"INSERT INTO slave VALUES ($1, $2, $3, $4, $5);",
+                formatted_time12, slave, money, k1, str(der)
+            )
+
+        if formatted_time2 > now:
+            await conn.execute(
+             f"INSERT INTO slave VALUES ($1, $2, $3, $4, $5);",
+                formatted_time23, slave, money, k2, str(der)
+            )
+
+        if formatted_time3 > now:
+            await conn.execute(
+                f"INSERT INTO slave VALUES ($1, $2, $3, $4, $5);",
+                formatted_time34, slave, money, k3, str(der)
+            )
+        return re, fer, w
+    else:
+        print(f"Не правильно {der}")
